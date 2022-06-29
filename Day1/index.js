266045c978ba766dd342bb49bccd17065e18b0da
VanillaDelight <2m5@naver.com>
2022-06-29 23:57:58 +0900

    공부

---
+++ Day1/index.js
@@ -0,0 +1,28 @@
+console.log('Hello World');
+
+let notebookPrice = 3000;
+
+console.log(notebookPrice);
+
+function greetings(sentense) {
+  console.log('hi');
+  console.log('하이');
+  console.log(sentense)
+};
+
+greetings('hola');
+
+function welcome(name) {
+  console.log('안녕하세요 ' + name + '님!');
+}
+
+welcome('홍깅')
+
+function printSquare(x) {
+  console.log(x*x);
+}
+
+printSquare(5);
+
+console.log('한국 영화 역사상 아카데미상을 받은 것은 \'기생충\'이 처음이다.');
+console.log('아리스토텔레스는 "인간은 사회적 동물이다."라고 말했다.');
